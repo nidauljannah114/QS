a927fb7641147925a0d2c4a107a4aee3180ea0d4
nidauljannah114 <nidajannah114@gmail.com>
2022-08-25 10:31:56 +0700

    latihan nida

---
+++ novice/02-04/5.1. More on Lists/5.1.1. Using Lists as Stacks/index.py
@@ -0,0 +1,23 @@
+fruits = ['orange', 'apple', 'pear', 'banana', 'kiwi', 'apple', 'banana']
+# print(fruits.count('apple'))
+# print(fruits.count('banana'))
+# print(fruits.count('kiwi'))
+# print(fruits.index('banana'))
+# print(fruits.index('banana', 4))
+# fruits.reverse()
+# print(fruits)
+# fruits.append('grape')
+# print(fruits)
+# fruits.sort()
+# print(fruits)
+# print(fruits.pop())
+
+stack = [3, 4, 5]
+stack.append(6)
+stack.append(7)
+print(stack)
+print(stack.pop())
+print(stack)
+print(stack.pop())
+print(stack.pop())
+print(stack)
