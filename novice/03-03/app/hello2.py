fc322672f4312e3923a676a2cc4f2c263ae365a1
nidauljannah114 <nidajannah114@gmail.com>
2022-08-31 12:28:13 +0700

    add request

--- novice/03-03/app/hello2.py
+++ novice/03-03/app/hello2.py
@@ -1,14 +1,15 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, request
 from markupsafe import escape
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
+@app.route('login', methods= ('GET', 'POST'))
+def login():
     return 'index'
 
 @app.route('/login')
 def login():
+    print(request)
     return 'login'
 
 @app.route('/user/<username>')
@@ -20,3 +21,11 @@ with app.test_request_context():
     print(url_for('login'))
     print(url_for('login', next='/'))
     print(url_for('profile', username='John Doe'))
+
+
+'''
+Create = Post
+Read = GET
+Update = PUT
+Delete = DELETE
+'''
