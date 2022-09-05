a0e54105afcd330e80442ab94d1050a961c12180
nidauljannah114 <nidajannah114@gmail.com>
2022-09-05 09:26:19 +0700

    update

--- novice/03-05/app/main.py
+++ novice/03-05/app/main.py
@@ -34,5 +34,23 @@ def index():
     # data = ["apel", "pear", "anggur"]
     return render_template("index.html", context = data)
 
+@app.route("/detail/<buah_id>")
+def detail(buah_id):
+    conn = psycopg2.connect(
+            host="localhost",
+            database="contoh",
+            user="postgres",
+            password="nida"
+    )
+    curs = conn.cursor()
+    query = f"select * from buah where id = (buah_id)"
+    curs.execute(query)
+    data = curs.fetchall()
+    # conn.close()
+    curs.close()
+    conn.close()
+    print(data)
+    return""
+
 if __name__== "__main__":
     app.run(host="0.0.0.0", port=8080)
