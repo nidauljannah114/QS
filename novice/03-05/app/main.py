79ddbb697cd4b7144fbb5fd861b7e81784821118
nidauljannah114 <nidajannah114@gmail.com>
2022-09-05 12:03:48 +0700

    update

--- novice/03-05/app/main.py
+++ novice/03-05/app/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 import psycopg2
 
 app = Flask(__name__)
@@ -43,14 +43,33 @@ def detail(buah_id):
             password="nida"
     )
     curs = conn.cursor()
-    query = f"select * from buah where id = (buah_id)"
+    query = f"select * from buah where id = {buah_id}"
     curs.execute(query)
-    data = curs.fetchall()
+    data = curs.fetchone()
     # conn.close()
     curs.close()
     conn.close()
     print(data)
-    return""
+    return render_template("detail.html", context=data)
+
+@app.route("/delete/<buah_id>")
+def delete(buah_id):
+    conn = psycopg2.connect(
+            host="localhost",
+            database="contoh",
+            user="postgres",
+            password="nida"
+    )
+    curs = conn.cursor()
+    # if request.method == "POST":
+    #     nama = request.form.get("nama")
+    #     detail = request.form.get("detail")
+    query = f"delete from buah where id = {buah_id}"
+    curs.execute(query)
+    conn.commit()
+    curs.close()
+    conn.close()
+    return redirect("/")
 
 if __name__== "__main__":
     app.run(host="0.0.0.0", port=8080)
