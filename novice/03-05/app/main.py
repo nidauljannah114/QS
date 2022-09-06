3adc4687509da420567390bd5581feaa77423ffa
nidauljannah114 <nidajannah114@gmail.com>
2022-09-06 08:53:32 +0700

    update

--- novice/03-05/app/main.py
+++ novice/03-05/app/main.py
@@ -71,5 +71,28 @@ def delete(buah_id):
     conn.close()
     return redirect("/")
 
+@app.route("/update/<buah_id>")
+def update(buah_id):
+    conn = psycopg2.connect(
+            host="localhost",
+            database="contoh",
+            user="postgres",
+            password="nida"
+    )
+    
+    curs = conn.cursor()
+
+    namaLama = 'mangga'
+    
+    namaBaru = 'alpukat'
+    detailBaru = 'manis'
+    query = f"update buah set nama='{namaBaru}', detail='{detailBaru}' where nama='{namaLama}'"
+    curs.execute(query)
+    conn.commit()
+    print("data masuk")
+
+    return redirect("/")
+
+
 if __name__== "__main__":
     app.run(host="0.0.0.0", port=8080)
